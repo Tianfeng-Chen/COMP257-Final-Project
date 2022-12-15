2262b5c3942729c6db98041bb12d55a9eaf9e234
Tianfeng Chen <t_chen21@u.pacific.edu>
2022-12-14 20:11:59 -0800

    add more test cases and necessary docs

--- LongestIncreasingSubsequence/src/test/java/LongestIncreasingSubsequenceTests.java
+++ LongestIncreasingSubsequence/src/test/java/LongestIncreasingSubsequenceTests.java
@@ -11,6 +11,15 @@ public class LongestIncreasingSubsequenceTests {
     private int[] testCase3;
     private int[] testCase4;
     private int[] testCase5;
+    private int[] testCase6;
+    private int[] testCase7;
+    private int[] testCase8;
+    private int[] testCase9;
+    private int[] testCase10;
+    private int[] testCase11;
+    private int[] testCase12;
+    private int[] testCase13;
+    private int[] testCase14;
     private LongestIncreasingSubsequenceController longestIncreasingSubsequenceController;
 
     @BeforeAll
@@ -20,6 +29,15 @@ public class LongestIncreasingSubsequenceTests {
         testCase3 = TestCases.testCase3;
         testCase4 = TestCases.testCase4;
         testCase5 = TestCases.testCase5;
+        testCase6 = TestCases.testCase6;
+        testCase7 = TestCases.testCase7;
+        testCase8 = TestCases.testCase8;
+        testCase9 = TestCases.testCase9;
+        testCase10 = TestCases.testCase10;
+        testCase11 = TestCases.testCase11;
+        testCase12 = TestCases.testCase12;
+        testCase13 = TestCases.testCase13;
+        testCase14 = TestCases.testCase14;
         longestIncreasingSubsequenceController = new LongestIncreasingSubsequenceController();
     }
 
@@ -94,6 +112,114 @@ public class LongestIncreasingSubsequenceTests {
         String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase5);
         System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
         String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase5);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_6_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(10, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase6), "DynamicProgramming: ");
+        Assertions.assertEquals(10, longestIncreasingSubsequenceController.runGreedySolution(testCase6), "Greedy: ");
+        System.out.println("TestCase6 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase6);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase6);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_7_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(14, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase7), "DynamicProgramming: ");
+        Assertions.assertEquals(14, longestIncreasingSubsequenceController.runGreedySolution(testCase7), "Greedy: ");
+        System.out.println("TestCase7 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase7);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase7);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_8_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(14, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase8), "DynamicProgramming: ");
+        Assertions.assertEquals(14, longestIncreasingSubsequenceController.runGreedySolution(testCase8), "Greedy: ");
+        System.out.println("TestCase8 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase8);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase8);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_9_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(15, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase9), "DynamicProgramming: ");
+        Assertions.assertEquals(15, longestIncreasingSubsequenceController.runGreedySolution(testCase9), "Greedy: ");
+        System.out.println("TestCase9 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase9);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase9);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_10_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(15, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase10), "DynamicProgramming: ");
+        Assertions.assertEquals(15, longestIncreasingSubsequenceController.runGreedySolution(testCase10), "Greedy: ");
+        System.out.println("TestCase10 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase10);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase10);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_11_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(19, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase11), "DynamicProgramming: ");
+        Assertions.assertEquals(19, longestIncreasingSubsequenceController.runGreedySolution(testCase11), "Greedy: ");
+        System.out.println("TestCase11 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase11);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase11);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_12_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(19, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase12), "DynamicProgramming: ");
+        Assertions.assertEquals(19, longestIncreasingSubsequenceController.runGreedySolution(testCase12), "Greedy: ");
+        System.out.println("TestCase12 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase12);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase12);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_13_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(27, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase13), "DynamicProgramming: ");
+        Assertions.assertEquals(27, longestIncreasingSubsequenceController.runGreedySolution(testCase13), "Greedy: ");
+        System.out.println("TestCase13 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase13);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase13);
+        System.out.println("Greedy: " + runTime3 + " nanoseconds.");
+        System.out.println();
+    }
+
+    @Test
+    public void TestCase_14_Should_Have_Expected_Result_For_All_Algorithms(){
+        Assertions.assertEquals(34, longestIncreasingSubsequenceController.runDynamicProgrammingSolution(testCase14), "DynamicProgramming: ");
+        Assertions.assertEquals(34, longestIncreasingSubsequenceController.runGreedySolution(testCase14), "Greedy: ");
+        System.out.println("TestCase14 Runtime: ");
+        String runTime2 = longestIncreasingSubsequenceController.getDynamicProgrammingRuntime(testCase14);
+        System.out.println("DynamicProgramming: " + runTime2 + " nanoseconds.");
+        String runTime3 = longestIncreasingSubsequenceController.getGreedyRuntime(testCase14);
         System.out.println("Greedy: " + runTime3 + " nanoseconds.");
         System.out.println();
     }
